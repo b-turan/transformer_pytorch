9e48bbb87fbe9c2d8cc8a5e3ae5e32993e01d141
b-turan <berkant.f.turan@gmail.com>
2022-06-28 17:23:06 +0200

    remove epoch funcs

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from pre_processor.pre_processor import _prepare_ds, get_dataloader
+from training_func.epoch import get_bleu_score, train_epoch, validation_epoch
 from utils import arg_parser, utils
 
 device = th.device('cuda' if th.cuda.is_available() else 'cpu')
@@ -43,7 +44,7 @@ def main():
     train_dataloader, validation_dataloader, _ = get_dataloader(train_ds, validation_ds, test_ds, 
                                                                 batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
     # Model Initialization
-    model = build_model(MODEL, IS_PRETRAINED)
+    model = utils.build_model(MODEL, IS_PRETRAINED, device)
     optimizer = th.optim.Adam(model.parameters(), lr = LEARNING_RATE)
     num_training_steps = N_EPOCHS * len(train_dataloader)
     lr_scheduler = transformers.get_scheduler(name="linear", optimizer=optimizer, 
@@ -59,8 +60,8 @@ def main():
         for epoch in range(N_EPOCHS):
             start_time = time.time()
 
-            train_loss = train_epoch(model, train_dataloader, optimizer, lr_scheduler, CLIP, tokenizer)
-            valid_loss = validation_epoch(model, validation_dataloader)
+            train_loss = train_epoch(model, train_dataloader, optimizer, lr_scheduler, CLIP, tokenizer, device)
+            valid_loss = validation_epoch(model, validation_dataloader, device)
             
             end_time = time.time()
             epoch_mins, epoch_secs = utils.epoch_time(start_time, end_time)
@@ -71,7 +72,7 @@ def main():
 
             # Bleu Score
             print(40*'-' + 'Calculate Bleu Score ' + 40*'-')
-            sacre_bleu_score = get_bleu_score(model, validation_dataloader, tokenizer)
+            sacre_bleu_score = get_bleu_score(model, validation_dataloader, tokenizer, device)
             print(f'\t SacreBleu Score: {sacre_bleu_score:.3f}')
 
             # logging
@@ -80,119 +81,11 @@ def main():
             writer.add_scalar("BLEU Score", sacre_bleu_score, epoch)
         writer.flush()
     else:
-        valid_loss = validation_epoch(model, validation_dataloader, tokenizer)
+        valid_loss = validation_epoch(model, validation_dataloader, tokenizer, device)
         # Bleu Score
         print(40*'-' + 'Calculate Bleu Score ' + 40*'-')
-        sacre_bleu_score = get_bleu_score(model, validation_dataloader, tokenizer)
+        sacre_bleu_score = get_bleu_score(model, validation_dataloader, tokenizer, device)
         print(f'\t SacreBleu Score: {sacre_bleu_score:.3f}')
-
-
-def build_model(model_name, IS_PRETRAINED):
-    '''
-    Returns T5 model (pretrained or randomly initialized)
-    '''
-    if IS_PRETRAINED:
-        # TODO: check alternative loading with AutoModel.from_pretrained()
-        model = transformers.T5ForConditionalGeneration.from_pretrained(model_name, 
-                                                                        torch_dtype="auto"
-                                                                        ).to(device)
-    else:
-        config = transformers.AutoConfig.from_pretrained(model_name) # see transformers/issues/14674
-        model = transformers.T5ForConditionalGeneration(config).to(device)
-    return model
-
-
-def train_epoch(model, train_dataloader, optimizer, lr_scheduler, CLIP, tokenizer):
-    '''
-    Trains model on the entire dataset for one epoch.
-    ------------------------------------
-    model (nn.model): Torch model
-    train_dataloader (torch.dataloader): Dataloader
-    optimizer (th.optim): Optimizer
-    CLIP (int): Gradient Clipping
-    ------------------------------------
-    returns average epoch loss
-    '''
-    model.train()
-    epoch_loss = 0
-    for batch in tqdm(train_dataloader):
-            src_ids = batch['src_ids'].to(device)
-            trg_ids = batch['trg_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-
-            trg_ids = th.tensor(trg_ids)
-
-            trg_ids[trg_ids == tokenizer.pad_token_id] = -100
-            loss = model(input_ids=src_ids, attention_mask=attention_mask, labels=trg_ids).loss     
-            loss.backward()
-            th.nn.utils.clip_grad_norm_(model.parameters(), CLIP)
-            optimizer.step()
-            lr_scheduler.step()
-            optimizer.zero_grad()
-            epoch_loss += loss.item()
-            print(lr_scheduler.get_last_lr())
-    return epoch_loss / len(train_dataloader)
-
-
-def validation_epoch(model, dataloader):
-    '''
-    Evaluates model on the entire dataset.
-    ------------------------------------
-    model (nn.model): Torch model
-    dataloader (torch.dataloader): Dataloader
-    tokenizer (transformer.tokenizer): Tokenizer
-    ------------------------------------
-    returns average epoch validation loss
-    '''
-    model.eval()
-    epoch_loss = 0
-    with th.no_grad():
-        for i, batch in enumerate(dataloader):
-            src_ids = batch['src_ids'].to(device)
-            trg_ids = batch['trg_ids'].to(device)
-            attention_mask = batch['attention_mask'].to(device)
-            loss = model(input_ids=src_ids, attention_mask=attention_mask, labels=trg_ids).loss     
-            epoch_loss += loss.item()
-    return epoch_loss / len(dataloader)
-
-
-def get_bleu_score(model, dataloader, tokenizer):
-    '''
-    Calculates sacrebleu score of given model on entire dataset.
-    ------------------------------------
-    model (nn.model): Torch model
-    dataloader (torch.dataloader): Dataloader
-    tokenizer (transformer.tokenizer): Tokenizer
-    ------------------------------------
-    Returns sacre_bleu_score
-    '''
-    # evaluate bleu score  
-    # TODO: can tokenizer be used with cuda?
-    model.eval()
-    sacre_bleu_score = 0
-    for batch in tqdm(dataloader):
-        src_ids = batch['src_ids'].to(device)
-        trg_ids = batch['trg_ids'].to(device)
-        attention_mask = batch['attention_mask'].to(device)
-        pred_seq = model.generate(src_ids,
-                                # attention_mask = attention_mask,
-                                # do_sample=True, 
-                                # top_p=0.84, 
-                                # top_k=100, 
-                                # max_length=32
-                                ) # encoded translation of src sentences
-        trg_decoded = tokenizer.batch_decode(trg_ids, skip_special_tokens=True) # decoded trg sentences 
-        pred_seq_decoded = tokenizer.batch_decode(pred_seq, skip_special_tokens=True) # decoded output translation 
-        
-        # original paper of SacreBLEU by Matt Post:
-        # https://arxiv.org/pdf/1804.08771.pdf
-        # additional material: 
-        # hugging face on (sacre)BLEU score https://www.youtube.com/watch?v=M05L1DhFqcw
-        sacre_bleu = datasets.load_metric('sacrebleu') # 'bleu' as input also possible
-        pred_list = [[sentence] for sentence in pred_seq_decoded]
-        trg_list = [[sentence] for sentence in trg_decoded]
-        sacre_bleu_score += sacre_bleu.compute(predictions=pred_list, references=trg_list)['score']
-    return sacre_bleu_score/len(dataloader)
 
 
 if __name__ == '__main__':
