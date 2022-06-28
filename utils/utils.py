d11e090ce63d4120ab6e799e38e9a01c7671986d
b-turan <berkant.f.turan@gmail.com>
2022-06-28 17:22:29 +0200

    add build_model

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,10 @@
+import transformers
+
+
 def count_parameters(model):
     """ Counts the learnable parameters of a given model """
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
 
 def epoch_time(start_time, end_time):
     ''' Returns elapsed time in minutes and seconds. '''
@@ -8,3 +12,19 @@ def epoch_time(start_time, end_time):
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
     return elapsed_mins, elapsed_secs
+
+
+def build_model(model_name, IS_PRETRAINED, device):
+    '''
+    Returns T5 model (pretrained or randomly initialized)
+    '''
+    if IS_PRETRAINED:
+        # TODO: check alternative loading with AutoModel.from_pretrained()
+        model = transformers.T5ForConditionalGeneration.from_pretrained(model_name, 
+                                                                        torch_dtype="auto"
+                                                                        ).to(device)
+    else:
+        config = transformers.AutoConfig.from_pretrained(model_name) # see transformers/issues/14674
+        model = transformers.T5ForConditionalGeneration(config).to(device)
+    return model
+
