b43fedeb38dfb5f44b970a5bfec000dddc3aa944
b-turan <berkant.f.turan@gmail.com>
2022-08-12 08:59:37 +0200

    add docstrings generated automatically

--- utils/utils.py
+++ utils/utils.py
@@ -2,12 +2,27 @@ import transformers
 
 
 def count_parameters(model):
-    """Counts the learnable parameters of a given model"""
+    """Counts the learnable parameters of a given model
+
+    Args:
+        model (torch.model): Model defined by PyTorch/HuggingFace
+
+    Returns:
+        int: Number of learnable parameters
+    """
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
 
 
-def epoch_time(start_time, end_time):
-    """Returns elapsed time in minutes and seconds."""
+def epoch_time(start_time: float, end_time: float) -> tuple[float, float]:
+    """Returns elapsed time in minutes and seconds.
+
+    Args:
+        start_time (float): Starting Time
+        end_time (float): End Time
+
+    Returns:
+        tuple[float, float]: Elapsed Minutes, Elapsed Seconds
+    """
     elapsed_time = end_time - start_time
     elapsed_mins = int(elapsed_time / 60)
     elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
